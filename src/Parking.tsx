ec64f5e77c66ac39f24ef9b2b7745270a634c625
szkj-web-he <xuejie.he@datareachable.com>
2022-09-06 15:42:24 +0800

    1

--- src/Parking.tsx
+++ src/Parking.tsx
@@ -1,6 +1,6 @@
 /* <------------------------------------ **** DEPENDENCE IMPORT START **** ------------------------------------ */
 /** This section will include all the necessary dependence for this tsx file */
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { comms } from ".";
 import { Drag } from "./Drag";
 import { DragBox } from "./DragBox";
@@ -20,35 +20,63 @@ const Temp: React.FC<TempProps> = ({ list, activeId, handleDragMove, handleDragE
     /* <------------------------------------ **** STATE START **** ------------------------------------ */
     /************* This section will include this component HOOK function *************/
 
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    const [colNumber, setColNumber] = useState(6);
+
+    const rows = useMemo(() => {
+        let arr: Array<Array<ParkingProps>> = [];
+        if (list.length > colNumber) {
+            let index = -1;
+            for (let i = 0; i < list.length; i++) {
+                const item = list[i];
+                if (!(i % colNumber)) {
+                    ++index;
+                    arr[index] = [item];
+                } else {
+                    arr[index].push(item);
+                }
+            }
+        } else {
+            arr = [list];
+        }
+        return arr;
+    }, [colNumber, list]);
     /* <------------------------------------ **** STATE END **** ------------------------------------ */
     /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
     /************* This section will include this component parameter *************/
+    useEffect(() => {
+        const rowsNumber = () => {
+            const el = ref.current;
+            if (!el) {
+                return;
+            }
 
+            const width = el.offsetWidth;
+
+            let cols = Math.floor(width / 150);
+            if (cols > 6) {
+                cols = 6;
+            }
+            setColNumber(cols);
+        };
+
+        rowsNumber();
+        window.addEventListener("resize", rowsNumber);
+        return () => {
+            window.removeEventListener("resize", rowsNumber);
+        };
+    }, []);
     /* <------------------------------------ **** PARAMETER END **** ------------------------------------ */
     /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
     /************* This section will include this component general function *************/
-    let arr: Array<Array<ParkingProps>> = [];
-    if (list.length > 6) {
-        let index = -1;
-        for (let i = 0; i < list.length; i++) {
-            const item = list[i];
-            if (!(i % 6)) {
-                ++index;
-                arr[index] = [item];
-            } else {
-                arr[index].push(item);
-            }
-        }
-    } else {
-        arr = [list];
-    }
 
     const content = (
         <div className="parking_main">
             <div className="parking_head">{comms.config.optionsInstruction}</div>
 
             <div className="parking_container">
-                {arr.map((row, n) => {
+                {rows.map((row, n) => {
                     const active = row.some((item) => item.id === activeId);
 
                     return (
@@ -63,10 +91,10 @@ const Temp: React.FC<TempProps> = ({ list, activeId, handleDragMove, handleDragE
                                 }
 
                                 let width: string;
-                                if (list.length > 6) {
-                                    width = "calc(100% / 6)";
+                                if (list.length > colNumber) {
+                                    width = `calc(100% / ${colNumber})`;
 
-                                    if (n < 5) {
+                                    if (n < colNumber - 1) {
                                         classList.push("parking_item__border");
                                     }
                                 } else {
@@ -87,7 +115,7 @@ const Temp: React.FC<TempProps> = ({ list, activeId, handleDragMove, handleDragE
                                         }}
                                     >
                                         <div className="parking_placeholder">
-                                            {n * 6 + index + 1}
+                                            {n * colNumber + index + 1}
                                         </div>
                                         {item.value && (
                                             <Drag
@@ -130,7 +158,11 @@ const Temp: React.FC<TempProps> = ({ list, activeId, handleDragMove, handleDragE
     );
 
     /* <------------------------------------ **** FUNCTION END **** ------------------------------------ */
-    return <div className="parking_wrap">{content}</div>;
+    return (
+        <div className="parking_wrap" ref={ref}>
+            {content}
+        </div>
+    );
 };
 /* <------------------------------------ **** FUNCTION COMPONENT END **** ------------------------------------ */
 export default Temp;
